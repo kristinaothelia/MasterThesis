7b2df174b0857e13fc51f1093158419392d58bf3
kristinaothelia <>
2021-11-14 00:19:51 +0100

    datasetinfo

--- train_efficientnet.py
+++ train_efficientnet.py
@@ -78,7 +78,7 @@ valid_loader = DatasetLoader(container=valid, transforms=valid_transforms)
 
 train_loader = torch.utils.data.DataLoader(dataset      = train_loader,
                                            num_workers  = 4,
-                                           batch_size   = 16,
+                                           batch_size   = 8,
                                            shuffle      = True,
                                            )
 valid_loader = torch.utils.data.DataLoader(dataset      = valid_loader,
