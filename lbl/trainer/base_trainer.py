ba4738a595634c313878c9d0e789ff2a86782316
kristinaothelia <>
2021-11-15 00:31:53 +0100

    train

--- lbl/trainer/base_trainer.py
+++ lbl/trainer/base_trainer.py
@@ -92,19 +92,18 @@ class BaseTrainer:
             v_acc.append(valid_acc)
 
             if epoch % self.save_period == 0:
-                #self.save_checkpoint(epoch, best=False)
-                print("slett")
+                self.save_checkpoint(epoch, best=False)
 
             #if val_loss < self.min_validation_loss:
             if 1 - valid_acc < self.min_validation_loss:
                 self.min_validation_loss = 1 - valid_acc
-                #self.save_checkpoint(epoch, best=True)
+                self.save_checkpoint(epoch, best=True)
                 best_ep = epoch
                 best_acc = valid_acc
 
             print('-----------------------------------')
 
-        #self.save_checkpoint(epoch, best=False)
+        self.save_checkpoint(epoch, best=False)
         print("ep: ", best_ep, " acc: ", best_acc)
 
         train_end_time = time.time() - train_time
--- train_efficientnet.py
+++ train_efficientnet.py
@@ -118,12 +118,12 @@ model_name = ['efficientnet-b0',
               'efficientnet-b3',
               'efficientnet-b4']
 
-train(json_file, model_name[0], ep=2, batch_size_train=8, learningRate=2e-3, stepSize=75, g=0.1)
-train(json_file, model_name[0], ep=100, batch_size_train=16, learningRate=2e-3, stepSize=75, g=0.1)
+#train(json_file, model_name[0], ep=100, batch_size_train=8, learningRate=2e-3, stepSize=75, g=0.1)
+#train(json_file, model_name[0], ep=100, batch_size_train=16, learningRate=2e-3, stepSize=75, g=0.1)
 
 #train(json_file, model_name[2], ep=100, batch_size_train=8, learningRate=2e-3, stepSize=75, g=0.1)
 #train(json_file, model_name[2], ep=100, batch_size_train=16, learningRate=2e-3, stepSize=75, g=0.1)
 #train(json_file, model_name[2], ep=100, batch_size_train=24, learningRate=2e-3, stepSize=75, g=0.1)
 
-#train(json_file, model_name[4], ep=100, batch_size_train=8, learningRate=2e-3, stepSize=75, g=0.1)
+train(json_file, model_name[4], ep=100, batch_size_train=8, learningRate=2e-3, stepSize=75, g=0.1)
 #train(json_file, model_name[4], ep=100, batch_size_train=16, learningRate=2e-3, stepSize=75, g=0.1)
