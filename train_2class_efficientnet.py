276c6534e73bd19cb1122035df463fbf6568c6dc
kristinaothelia <>
2022-01-03 03:27:53 +0100

    2 classes

--- train_2class_efficientnet.py
+++ train_2class_efficientnet.py
@@ -16,102 +16,128 @@ from lbl.preprocessing import (
     StandardizeNonZero,
     )
 # -----------------------------------------------------------------------------
-container = DatasetContainer.from_json('datasets/Full_aurora_ml_corr.json')
+#container = DatasetContainer.from_json('datasets/Full_aurora_ml_corr.json')
 
-def remove_noLabel_img(container):
-    """ Remove images with no label """
+def train(container, batch_s):
 
-    length = len(container)
-    print('original container length:   ', length)
-    counter = 0
-    for i in range(length):
-        i -= counter
-        if container[i].label == None:
-            del container[i]
-            counter += 1
-    print('removed images with no label: ', counter)
-    return container
+    def remove_noLabel_img(container):
+        """ Remove images with no label """
 
-container = remove_noLabel_img(container)
+        length = len(container)
+        print('original container length:   ', length)
+        counter = 0
+        for i in range(length):
+            i -= counter
+            if container[i].label == None:
+                del container[i]
+                counter += 1
+        print('removed images with no label: ', counter)
+        return container
 
-train, valid = container.split(seed=42, split=0.8)
+    container = remove_noLabel_img(container)
 
-model_name = 'efficientnet-b0'
-#model_name = 'efficientnet-b1'
-#model_name = 'efficientnet-b2'
-#model_name = 'efficientnet-b3'
-#model_name = 'efficientnet-b4'
+    train, valid = container.split(seed=42, split=0.8)
 
-img_size = efficientnet_params(model_name)['resolution']
+    model_name = 'efficientnet-b0'
+    #model_name = 'efficientnet-b1'
+    #model_name = 'efficientnet-b2'
+    #model_name = 'efficientnet-b3'
+    #model_name = 'efficientnet-b4'
 
-# rotation class: numpy arrays. Padding class: pytorch tensors
-train_transforms = torchvision.transforms.Compose([
-    lambda x: np.float32(x),
-    lambda x: torch.from_numpy(x),
-    lambda x: x.unsqueeze(0),
-    RotateCircle,
-    lambda x: torch.nn.functional.interpolate(
-            input=x.unsqueeze(0),
-            size=img_size,
-            mode='bicubic',
-            align_corners=True,
-            ).squeeze(0),
-    StandardizeNonZero(),
-    # PadImage(size=480),
-    ])
+    img_size = efficientnet_params(model_name)['resolution']
 
-# No need to rotate validation images
-valid_transforms = torchvision.transforms.Compose([
-    lambda x: np.float32(x),
-    lambda x: torch.from_numpy(x),
-    lambda x: x.unsqueeze(0),
-    lambda x: torch.nn.functional.interpolate(
-            input=x.unsqueeze(0),
-            size=img_size,
-            mode='bicubic',
-            align_corners=True,
-            ).squeeze(0),
-    StandardizeNonZero(),
-    # PadImage(size=480),
-    ])
+    def count(container):
 
+        aurora_less = 0; aurora = 0
 
-train_loader = DatasetLoader_2class(container=train, transforms=train_transforms)
-valid_loader = DatasetLoader_2class(container=valid, transforms=valid_transforms)
+        for i in range(len(container)):
+            if train[i].label == 'aurora-less':
+                aurora_less += 1
+            if train[i].label == 'aurora':
+                aurora += 1
 
-train_loader = torch.utils.data.DataLoader(dataset      = train_loader,
-                                           num_workers  = 4,
-                                           batch_size   = 12,
-                                           shuffle      = True,
-                                           )
-valid_loader = torch.utils.data.DataLoader(dataset      = valid_loader,
-                                           num_workers  = 4,
-                                           batch_size   = 1,
-                                           shuffle      = False,
-                                           )
+        return aurora_less, aurora
 
-model = EfficientNet.from_name(model_name=model_name, num_classes=2, in_channels=1)
+    aurora_less, aurora = count(train)
+    print("class count, train: ", [aurora_less, aurora])
 
-model_parameters = filter(lambda p: p.requires_grad, model.parameters())
-params = sum([np.prod(p.size()) for p in model_parameters])
-print('The number of params in Million: ', params/1e6)
+    class_weights = [aurora_less/aurora_less, aurora_less/aurora]
 
-loss         = torch.nn.CrossEntropyLoss()
-optimizer    = torch.optim.Adam(params=model.parameters(), lr=2e-3, amsgrad=True)
-lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer=optimizer, step_size=75, gamma=0.1)
+    # rotation class: numpy arrays. Padding class: pytorch tensors
+    train_transforms = torchvision.transforms.Compose([
+        lambda x: np.float32(x),
+        lambda x: torch.from_numpy(x),
+        lambda x: x.unsqueeze(0),
+        RotateCircle,
+        lambda x: torch.nn.functional.interpolate(
+                input=x.unsqueeze(0),
+                size=img_size,
+                mode='bicubic',
+                align_corners=True,
+                ).squeeze(0),
+        StandardizeNonZero(),
+        # PadImage(size=480),
+        ])
+
+    # No need to rotate validation images
+    valid_transforms = torchvision.transforms.Compose([
+        lambda x: np.float32(x),
+        lambda x: torch.from_numpy(x),
+        lambda x: x.unsqueeze(0),
+        lambda x: torch.nn.functional.interpolate(
+                input=x.unsqueeze(0),
+                size=img_size,
+                mode='bicubic',
+                align_corners=True,
+                ).squeeze(0),
+        StandardizeNonZero(),
+        # PadImage(size=480),
+        ])
 
 
-trainer = Trainer(model             = model,
-                  loss_function     = loss,
-                  optimizer         = optimizer,
-                  data_loader       = train_loader,
-                  valid_data_loader = valid_loader,
-                  lr_scheduler      = lr_scheduler,
-                  epochs            = 100,
-                  save_period       = 50,
-                  savedir           = './models/2class/{}'.format(model_name[-2:]),
-                  #savedir           = '/itf-fi-ml/home/koolsen/Master/',
-                  device            = 'cuda:0',
-                  )
+    train_loader = DatasetLoader_2class(container=train, transforms=train_transforms)
+    valid_loader = DatasetLoader_2class(container=valid, transforms=valid_transforms)
 
-trainer.train()
+    train_loader = torch.utils.data.DataLoader(dataset      = train_loader,
+                                               num_workers  = 4,
+                                               batch_size   = batch_s,
+                                               shuffle      = True,
+                                               )
+    valid_loader = torch.utils.data.DataLoader(dataset      = valid_loader,
+                                               num_workers  = 4,
+                                               batch_size   = 1,
+                                               shuffle      = False,
+                                               )
+
+    model = EfficientNet.from_name(model_name=model_name, num_classes=2, in_channels=1)
+
+    model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+    params = sum([np.prod(p.size()) for p in model_parameters])
+    print('The number of params in Million: ', params/1e6)
+
+    loss         = torch.nn.CrossEntropyLoss(weight=torch.tensor(class_weights))
+    #loss         = torch.nn.CrossEntropyLoss()
+    optimizer    = torch.optim.Adam(params=model.parameters(), lr=1e-3, amsgrad=True)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer=optimizer, step_size=150, gamma=0.1)
+
+
+    trainer = Trainer(model             = model,
+                      loss_function     = loss,
+                      optimizer         = optimizer,
+                      data_loader       = train_loader,
+                      valid_data_loader = valid_loader,
+                      lr_scheduler      = lr_scheduler,
+                      epochs            = 200,
+                      save_period       = 50,
+                      savedir           = './models/2class/{}/{}'.format(model_name[-2:], batch_s),
+                      #savedir           = '/itf-fi-ml/home/koolsen/Master/',
+                      device            = 'cuda:2',
+                      )
+
+    trainer.train()
+
+container = DatasetContainer.from_json('datasets/Full_aurora_ml_corr_NEW.json')
+
+train(container=container, batch_s=8)
+train(container=container, batch_s=16)
+train(container=container, batch_s=24)
