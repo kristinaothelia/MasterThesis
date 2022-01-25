73ecb748c15e0b00a2d21534b8dd76a4a61370ae
kristinaothelia <>
2022-01-25 20:09:04 +0100

    predict file edits

--- predict.py
+++ predict.py
@@ -168,7 +168,7 @@ mlnodes_path = '/itf-fi-ml/home/koolsen/Master/'
 # Load a saved model. UPDATE
 model_name = model_names[3]
 #model_path = "models/b2/2021-10-02/best_validation/checkpoint-best.pth"
-model_path = "models/report/b3_16/best_validation/checkpoint-best.pth"
+#model_path = "models/report/b3_16/best_validation/checkpoint-best.pth"
 model_path = "models/report/best_validation/checkpoint-best.pth"
 
 """
@@ -186,23 +186,25 @@ predict(model_name, model_path, container, LABELS, save_file, test=True)
 
 def Test(model_name, model_path, LABELS):
 
-    json_file = 'datasets/Full_aurora_test_set.json'
+    json_file = 'datasets/Full_aurora_test_set_ml.json'
     container = DatasetContainer.from_json(json_file)
     save_file = json_file[:-5]+'_predicted_'+model_name+'.json'
 
     predict(model_name, model_path, container, LABELS, save_file, test=True)
 
 
-def Predict_on_unlabeld_data(model_name, model_path, LABELS):
+def Predict_on_unlabeld_data(model_name, model_path, mlnodes_path, LABELS):
 
     json_file = 'datasets/Full_aurora_new_rt_ml.json'
     container = DatasetContainer.from_json(json_file)
+
+    #save_file = mlnodes_path+json_file[:-5]+'_predicted_'+model_name+'.json'
     save_file = json_file[:-5]+'_predicted_'+model_name+'_TESTNEW_.json'
 
     predict(model_name, model_path, container, LABELS, save_file)
 
 Test(model_name, model_path, LABELS)
-#Predict_on_unlabeld_data(model_name, model_path, LABELS)
+#Predict_on_unlabeld_data(model_name, model_path, mlnodes_path, LABELS)
 
 """
 # Load json file to add predictions
